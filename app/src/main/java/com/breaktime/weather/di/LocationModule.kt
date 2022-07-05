125d90cbf9e1b737bccfcd7f47a3e429b275e7bd
Elisey Budnitsky <bgs6544@gmail.com>
2022-07-05 05:21:23 +0300

    feat: setup di

---
+++ app/src/main/java/com/breaktime/weather/di/LocationModule.kt
@@ -0,0 +1,17 @@
+package com.breaktime.weather.di
+
+import com.breaktime.weather.data.location.DefaultLocationTracker
+import com.breaktime.weather.domain.location.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
---
+++ app/src/main/java/com/breaktime/weather/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.breaktime.weather.di
+
+import com.breaktime.weather.data.repository.WeatherRepositoryImpl
+import com.breaktime.weather.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(weatherRepositoryImpl: WeatherRepositoryImpl): WeatherRepository
+}
