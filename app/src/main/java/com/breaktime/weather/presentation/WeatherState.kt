8c00a659f2d1e810df41e0d6192ae64027002fd4
Elisey Budnitsky <bgs6544@gmail.com>
2022-07-05 05:31:47 +0300

    feat: add viewModel with state

---
+++ app/src/main/java/com/breaktime/weather/presentation/WeatherState.kt
@@ -0,0 +1,9 @@
+package com.breaktime.weather.presentation
+
+import com.breaktime.weather.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
